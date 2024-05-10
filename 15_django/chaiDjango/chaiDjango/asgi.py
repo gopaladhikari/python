4eda3a3270c9d9ff3617b604ffbef8761ed19713
gopaladhikari <gopuadks@gmail.com>
2024-05-10 21:35:49 +0545

    initialze django project

--- 15_django/chaiDjango/chaiDjango/asgi.py
+++ 15_django/chaiDjango/chaiDjango/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for project_01 project.
+ASGI config for chaiDjango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_01.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chaiDjango.settings')
 
 application = get_asgi_application()
--- 15_django/chaiDjango/chaiDjango/wsgi.py
+++ 15_django/chaiDjango/chaiDjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project_01 project.
+WSGI config for chaiDjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_01.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chaiDjango.settings')
 
 application = get_wsgi_application()
